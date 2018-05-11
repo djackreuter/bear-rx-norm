61cf805ee72a850cb0cf220c53092f313c05bc1f
Jack Reuter <djreuter45@gmail.com>
2018-05-11 09:24:12 -0600

    added index method for searching drugs

--- src/controllers/drugs_controller.cr
+++ src/controllers/drugs_controller.cr
@@ -1,5 +1,14 @@
 class DrugsController < ApplicationController
   def index
-    render "home/index"
+    drugs = NdcProducts.all
+    valid_params = params.validation do
+      required(:search, "Search something") { |_| true } 
+    end
+    if valid_params[:search]
+      drugs = NdcProducts.search(params[:search])
+    else
+      flash[:notice] = "Could not find that drug."
+      redirect_to("/drugs")
+    end
   end
 end
